d4e906a1f87bc252345cee898a540584e4c7e766
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-05-23 04:20:19 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -6,25 +6,23 @@ import TestComponent from './TestComponent'
 // Add your code own within the return statement
 
 class App extends Component {
-  render() {
 
+render() {
     return (
       <div className="App">
         <header className="App-header">
-          {moment().format('Now')}
+          <p>Now</p>
         </header>
         <p className="App-intro">
           In React apps, we write JSX - it looks like HTML, and uses a lot of HTML syntax.
           JSX lets us include JavaScript functions right along with the HTML, and also
           allows us to add in components, which are separate, self-contained chunks of JSX.
         </p>
-        <ExampleComponent />
-        <ExampleComponent />
         <TestComponent />
-
+        <ExampleComponent />
       </div>
     );
-  }
+}
 }
 
 export default App;
